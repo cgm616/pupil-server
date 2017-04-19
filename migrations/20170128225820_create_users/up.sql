7a920911d57dbc25d3a34b11c88e187acec09e6d
cgm616 <cgm616@me.com>
2017-04-18 22:24:00 -0500

    Make email and username both unique
    No two users should have the same email OR the same username. The others
    (password, name) should be fine.

--- migrations/20170128225820_create_users/up.sql
+++ migrations/20170128225820_create_users/up.sql
@@ -1,7 +1,7 @@
 create table users (
   id serial primary key,
   name varchar not null,
-  email varchar not null,
+  email varchar not null unique,
   username varchar not null unique,
   pass varchar not null,
   conf boolean not null default 'f'
